47d246588e693275c661aee50f139d62cdbf83f2
Softy7 <129875351+Softy7@users.noreply.github.com>
2023-09-21 20:14:06 +0200

    Delete Model directory
    Quelques réparations...

--- Model/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-ini_set("SMTP","smtp.google.com" );
-
-mail("ewanrecquignies@gmail.com", "Inscription", "Bonjour,\n Vous avez soumis votre inscription  
-        pour le tournoi de Chôlage. Nos administrateurs vont vérifier vos informations et valider ou réfuter votre 
-        inscription, le cas échéant, vous recevrez un mail de confirmation. Nous vous remercions de votre inscription et 
-        espérons vous recevoir très vite pour l'événement. \n\n Cordialement,\nL'équipe du tournoi !!!");
