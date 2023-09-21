2baf44f9fe483028b0c0647cd50476c4541bb705
Softy7 <ewanrecquignies@gmail.com>
2023-09-21 21:03:13 +0200

    On y va

---
+++ Model/PlayerAdministrator.php
@@ -0,0 +1,6 @@
+<?php
+class PlayerAdministrator extends Administrator {
+    function __construct($un, $m, $n, $fn, $b, $p) {
+        parent::__construct($un, $m, $n, $fn, $b, $p, false);
+    }
+}
