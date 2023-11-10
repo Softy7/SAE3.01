5b2b860c715501ceb9a3a96e0f253ba489e09097
Softy7 <129875351+Softy7@users.noreply.github.com>
2023-11-10 08:31:32 +0100

    Add files via upload

---
+++ Controller/AdminFunctions/CreateTeam.php
@@ -0,0 +1,13 @@
+<?php
+
+$TeamName = $_POST['nameTeam'];
+$Captain = $_POST['cap'];
+$Player1 = $_POST['Player1'];
+$Player2 = $_POST['Player2'];
+$Player3 = $_POST['Player3'];
+
+echo "nom d'équipe : $TeamName";
+echo "Capitaine : $Captain";
+echo "Joueur 1 : $Player1";
+echo "Joueur 2 : $Player2";
+echo "Joueur 3 : $Player3";
